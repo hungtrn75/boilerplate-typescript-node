adbdf1b5601f39af59abbbb95643e44c8593caa5
Hung Tran <hungtrn75@gmail.com>
2018-10-28 02:15:52 +0700

    SSL https

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import app from "./app";
+import * as https from "https";
+import * as fs from "fs";
 
 if (process.env.NODE_ENV !== "production") {
   const errorHandler = require("errorhandler");
   app.use(errorHandler());
 }
 
-const server = app.listen(app.get("port"), () => {
+const httpsOptions = {
+  key: fs.readFileSync("./config/key.pem"),
+  cert: fs.readFileSync("./config/cert.pem")
+};
+
+https.createServer(httpsOptions, app).listen(app.get("port"), () => {
   console.log(
-    "🚀 Server ready at http://localhost:%d in %s mode",
+    "🚀 Server ready at https://localhost:%d in %s mode",
     app.get("port"),
     app.get("env")
   );
   console.log("  Press CTRL-C to stop");
 });
-
-export default server;
